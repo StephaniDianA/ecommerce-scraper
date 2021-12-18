71bebd398157540fa354a81711e55e00e812d05b
Stephani Angelina <stephani.angelina91@gmail.com>
2021-12-18 16:49:32 +0700

    Initial Commit

---
+++ src/main/java/com/assigment/ecomscraper/EcomscraperApplication.java
@@ -0,0 +1,13 @@
+package com.assigment.ecomscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomscraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assigment/ecomscraper/EcomscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.ecomscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
